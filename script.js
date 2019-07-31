b3408ba8bb2cb5d53164124d77e4bb3e63e610b8
shruti gudi <shrutigudi@gmail.com>
2019-07-31 01:01:34 -0700

    added js, css and html changes

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
+// Generate data
+const generateNxN = (n, m) => {
+    const rows = [];
+    for (let i = 0; i < n; i++) {
+      const row = [];
+      for (let j = 0; j < m; j++) {
+        row.push('');
+      }
+      rows.push(row);
+    }
+    return rows;    
+}
+
+// Generate HTML for rows and cols
+const generateHTML = (rows)=> {
+    return rows.map(row =>
+    `<div class='row'>
+    ${ row.map(cell => `<div class ='cell'>${ cell }</div>`).join('')}
+    </div>`).join('');
+}
+
+const grid = document.querySelector('#gridContainer')
+
+// Render the grid
+const render = (rowVal, colVal) => {
+    const data = generateNxN(rowVal, colVal);
+    const html = generateHTML(data);
+    grid.innerHTML = html;
+}
+
 const formContainer = document.querySelector('#gridInput')
 const inputRows = document.querySelector('[name="rows"]')
 const inputColumns = document.querySelector('[name="columns"]')
 
+// Submit event
 formContainer.addEventListener('submit', (ev) => {
     ev.preventDefault()
     const rowVal = inputRows.value
@@ -9,15 +40,47 @@ formContainer.addEventListener('submit', (ev) => {
     render(rowVal, colVal)
 })
 
-const render = (rowVal, colVal) => {
-    const parentGrid = document.querySelector('#gridContainer')
-    const cell = document.createElement('div')
-    cell.classList.add('cell')
-
-    // render columns
-    i = 0
-    while (i < colVal) {
-        
-        i++
+// Displaying row and column index on cell hover
+grid.addEventListener('click', (ev) => {
+    const cell = ev.target
+    if (cell.className.includes("cell")) {
+        console.log("in click")
+        const row = cell.parentNode
+        const cdx = [...row.children].indexOf(cell)
+        const rdx = [...row.parentNode.children].indexOf(row)
+        const rowIdxEl = document.querySelector('#rowIdx')
+        const colIdxEl = document.querySelector('#colIdx')
+        ev.preventDefault()
+        rowIdxEl.innerText = "Row: " + rdx
+        colIdxEl.innerText = "Col: " + cdx
     }
-}
+})
+
+// Validation for rows input field
+inputRows.addEventListener('keyup', (ev) => {
+    ev.preventDefault()
+    const value = ev.target.value
+    const rowError = document.querySelector('#rowError')
+    if (!Number(value) || (value<0) || (value>50)) {
+        rowError.innerText = "row must only contain a number between 0 to 50"   
+    }
+    if (value === '' || ((value >= 0) && (value <=50))) {
+        rowError.innerText = ''
+    }
+})
+
+// Validation for columns input field
+inputColumns.addEventListener('keyup', (ev) => {
+    ev.preventDefault()
+    const value = ev.target.value
+    const ColError = document.querySelector('#colError')
+    if (!Number(value) || (value<0) || (value>50)) {
+        ColError.innerText = "column must only contain a number between 0 to 50"   
+    }
+    if (value === '' || ((value >= 0) && (value <=50))) {
+        ColError.innerText = ''
+    }
+})
+
+
+
